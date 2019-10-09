e60b3bb24cc1b9a4d6bd30ef75996b0f1dea397f
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2019-10-09 14:06:11 +0100

    making a very simple benchmarkcontroller (no web interface, just a python script)

--- benchmark_tools/controller/controller.py
+++ benchmark_tools/controller/controller.py
@@ -2,10 +2,6 @@
 import uuid
 import hashlib
 
-from flask import Flask, request, jsonify, make_response, abort
-
-app = Flask(__name__)
-
 
 # this is just a mocked method for running the benchmark
 def start_benchmark(benchmark, target_system):
@@ -18,17 +14,11 @@ def make_confs_id(benchmark, target_system):
     return confs_id
 
 
-@app.route('/api/v1.0/run_benchmark', methods=['post'])
-def run_benchmark():
-    if not request.json:
-        abort(400)
-
-    benchmark = request.json.get('benchmark')
-    target_system = request.json.get('target_system')
+def run_benchmark(benchmark, target_system):
     confs_id = make_confs_id(benchmark, target_system)
     run_id = str(uuid.uuid4())
     benchmark_results = start_benchmark(benchmark, target_system)
-    return make_response(jsonify({
+    return {
         'results': benchmark_results,
         'configs': {
             'confs_id': confs_id,
@@ -36,13 +26,4 @@ def run_benchmark():
             'target_system': target_system
         },
         'run_id': run_id
-    }), 200)
-
-
-@app.route('/', methods=['get'])
-def get_index():
-    return make_response(jsonify({}), 200)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    }
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,5 +1,3 @@
-import os
-import tempfile
 import unittest
 # from unittest.mock import patch
 
@@ -9,41 +7,20 @@ from benchmark_tools.controller import controller
 class ControllerTestCase(unittest.TestCase):
 
     def setUp(self):
-        # self.db_fd, controller.app.config['DATABASE'] = tempfile.mkstemp()
-
-        controller.app.config['TESTING'] = True
-        controller.app.config['WTF_CSRF_ENABLED'] = False
-        controller.app.config['DEBUG'] = False
-        controller.app.testing = True
-        self.client = controller.app.test_client()
-        # with controller.app.app_context():
-        #     pass
-
-    def test_main_page(self):
-        response = self.client.get('/', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+        pass
 
     def test_run_benchmark(self):
+        benchmark = {1: 2}
+        target_system = {3: 4}
 
-        payload = {
-            'benchmark': 1,
-            'target_system': 2
-        }
-
-        ret = self.client.post(
-            '/api/v1.0/run_benchmark',
-            json=payload,
-            follow_redirects=True,
-
-        )
-        self.assertEqual(ret.status_code, 200)
+        res = controller.run_benchmark(benchmark, target_system)
         for k in ['results', 'configs', 'run_id']:
-            self.assertIn(k, ret.json)
+            self.assertIn(k, res)
         for k in ['benchmark', 'target_system', 'confs_id']:
-            self.assertIn(k, ret.json['configs'])
+            self.assertIn(k, res['configs'])
 
-        self.assertEqual(payload['benchmark'], ret.json['configs']['benchmark'])
-        self.assertEqual(payload['target_system'], ret.json['configs']['target_system'])
+        self.assertDictEqual(benchmark, res['configs']['benchmark'])
+        self.assertDictEqual(target_system, res['configs']['target_system'])
 
     def tearDown(self):
         # os.close(self.db_fd)
