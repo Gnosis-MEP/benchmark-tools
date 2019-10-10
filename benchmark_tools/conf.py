bd8fae360513ee1635c1aaae5e71ea650d30525a
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2019-10-10 11:53:23 +0100

    fixing controller

--- benchmark_tools/conf.py
+++ benchmark_tools/conf.py
@@ -7,6 +7,6 @@ PROJECT_ROOT = os.path.dirname(SOURCE_DIR)
 
 LOGGING_LEVEL = config('LOGGING_LEVEL', default='DEBUG')
 
-BENCHMARK_JSON_CONFIG_PATH = config('BENCHMARK_JSON_CONFIG', default=os.path.join(PROJECT_ROOT, 'bm.json'))
-TARGET_SYSTEM_JSON_CONFIG_PATH = config('TARGET_SYSTEM_JSON_CONFIG', default=os.path.join(PROJECT_ROOT, 'ts.json'))
+BENCHMARK_JSON_CONFIG_PATH = config('BENCHMARK_JSON_CONFIG_PATH', default=os.path.join(PROJECT_ROOT, 'bm.json'))
+TARGET_SYSTEM_JSON_CONFIG_PATH = config('TARGET_SYSTEM_JSON_CONFIG_PATH', default=os.path.join(PROJECT_ROOT, 'ts.json'))
 RESULT_WEBHOOK_URL = config('BASE_RESULT_WEBHOOK_URL', default='http://localhost:5000/set_result/123-abc')
